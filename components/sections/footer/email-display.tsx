9d5c108ea16bcae9ed3f5ebbec342b3516a3bee6
Eaysin Arafat <eaysin.arafat.j@gmail.com>
2025-02-15 15:22:53 +0600

    change email address

--- components/sections/footer/email-display.tsx
+++ components/sections/footer/email-display.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { Typography } from "@/components/ui/typography";
-import { CheckIcon, ClipboardIcon } from "lucide-react";
-import { useState } from "react";
+} from '@/components/ui/tooltip';
+import { Typography } from '@/components/ui/typography';
+import { CheckIcon, ClipboardIcon } from 'lucide-react';
+import { useState } from 'react';
 
 const EmailDisplay = () => {
   const [isCopied, setIsCopied] = useState(false);
@@ -19,40 +19,40 @@ const EmailDisplay = () => {
       setIsCopied(true);
       setTimeout(() => setIsCopied(false), 2000); // Reset after 2 seconds
     } catch (err) {
-      console.error("Failed to copy: ", err);
+      console.error('Failed to copy: ', err);
     }
   };
 
   return (
-    <div className="flex items-center justify-center break-all">
+    <div className='flex items-center justify-center break-all'>
       <Tooltip>
         <TooltipTrigger asChild>
           <div
-            onClick={() => copyToClipboard("eaysin.arafat.j@gmail.com")}
-            className="cursor-pointer flex items-center space-x-2 relative my-7 "
+            onClick={() => copyToClipboard('eaysin.dev@gmail.com')}
+            className='cursor-pointer flex items-center space-x-2 relative my-7 '
           >
             <Typography
-              variant="inlineCode"
-              className="text-[2rem] md:text-[4rem] lg:text-[4rem] whitespace-pre-wrap text-center py-3 px-5"
+              variant='inlineCode'
+              className='text-[2rem] md:text-[4rem] lg:text-[4rem] whitespace-pre-wrap text-center py-3 px-5'
             >
-              eaysin.arafat.j@gmail.com
+              eaysin.dev@gmail.com
             </Typography>
             {isCopied ? (
               <CheckIcon
-                className="text-green-600 absolute top-2 right-2"
+                className='text-green-600 absolute top-2 right-2'
                 size={15}
               />
             ) : (
               <ClipboardIcon
-                className="text-gray-500 absolute top-2 right-2"
+                className='text-gray-500 absolute top-2 right-2'
                 size={14}
               />
             )}
           </div>
         </TooltipTrigger>
         <TooltipContent>
-          <Typography variant="small">
-            {isCopied ? "Copied!" : "Click to copy"}
+          <Typography variant='small'>
+            {isCopied ? 'Copied!' : 'Click to copy'}
           </Typography>
         </TooltipContent>
       </Tooltip>
